8b34e303dbc61d3bd557f91c8d88c92b557fc527
mauricio.idiart <mauricio.idiart@125a5b3a-3330-0410-a7dc-93bad6e629e8>
2007-05-11 15:21:29 +0000

    Initial import.

---
+++ uade/poo/ejemplo02/src/Sistema.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Sistema {
+	private Vector socios;
+	private Vector videos;
+	private Vector prestamos;
+	
+	public Sistema() {
+		this.socios = new Vector();
+		this.videos = new Vector();
+		this.prestamos = new Vector();
+	
+	}
+
+	
+}
