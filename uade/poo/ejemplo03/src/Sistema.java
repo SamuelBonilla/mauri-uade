c1f09734819f7bf71c644e007795e3c821beb5e0
mauricio.idiart <mauricio.idiart@125a5b3a-3330-0410-a7dc-93bad6e629e8>
2007-05-11 16:15:28 +0000

    Initial import.

---
+++ uade/poo/ejemplo03/src/Sistema.java
@@ -0,0 +1,15 @@
+import java.util.Vector;
+
+
+public class Sistema {
+	private Vector mensajes;
+	private Mensaje bienvenida;
+	
+	public Sistema() {
+		this.mensajes = new Vector();
+		
+		
+	}
+	
+	
+}
