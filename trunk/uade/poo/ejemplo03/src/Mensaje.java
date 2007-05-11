ad4aac6cb51046a997a48539cba2cb367f5548f7
mauricio.idiart <mauricio.idiart@125a5b3a-3330-0410-a7dc-93bad6e629e8>
2007-05-11 16:15:28 +0000

    Initial import.

---
+++ trunk/uade/poo/ejemplo03/src/Mensaje.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Mensaje {
+	private Date fecha;
+	private int telefono;
+	private String mensaje;
+	
+	public Mensaje(Date f, int tel, String msg) {
+		this.fecha=f;
+		this.telefono=tel;
+		this.mensaje= msg;
+		// TODO Auto-generated constructor stub
+	}
+
+}
